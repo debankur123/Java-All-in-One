12c46dc063bed890251da7062f4fb5a22f093033
debankur123 <debankurbhattacharjee784@gmail.com>
2022-10-09 14:15:51 +0530

    1st commit

---
+++ JAVA/src/isPrimeorNot.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class isPrimeorNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++){
+            int n = sc.nextInt();
+            int count  = 0;
+            for (int div=2;div*div<=n;div++){
+                if(n%div==0){
+                    count++;
+                    break;
+                }
+            }
+            if(count==0){
+                System.out.println("prime");
+            }
+            else {
+                System.out.println("Not prime");
+            }
+        }
+    }
+}
