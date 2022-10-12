d364e0bd216b64fed4eeaf1a635fa814f321f805
debankur123 <debankurbhattacharjee784@gmail.com>
2022-10-12 19:48:17 +0530

    Commit to master

--- JAVA/src/isPrimerNot.java
+++ JAVA/src/isPrimerNot.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-
-public class isPrimeorNot {
+public class isPrimerNot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
